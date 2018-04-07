f7fffded2e89cbbcbd8cc217417feea8b37b1a9a
Kenneth Barnes <kennethbarnes@Kenneths-MacBook-Air.local>
2018-04-06 22:25:41 -0400

    second input

---
+++ stalker.js
@@ -0,0 +1,11 @@
+var userFirstName = prompt("What is your first name? ");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+alert("Very nice to meet you, " + userFirstName + " " + userLastName + "!");
+
+console.log("I am pleased to meet you too, " + userFirstName + " " + userLastName);
+
+alert("You are " + userAge + " years old.");
+
+
